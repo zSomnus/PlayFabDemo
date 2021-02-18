4056d307db2d4f6ec4b178cc2ba7bc2a75382023
Ella <ellazhang37@gmail.com>
2021-02-18 09:41:43 -0400

    Initial commit

---
+++ Assets/Scripts/PlayFabLogin.cs
@@ -0,0 +1,29 @@
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+{
+    public void Start()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+        {
+            // If the title ID is not set in Unity Editor, this line will set the title ID;
+            PlayFabSettings.staticSettings.TitleId = "3ABCD";
+        }
+        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+    }
+
+    private void OnLoginFailure(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
